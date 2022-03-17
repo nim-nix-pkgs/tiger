b57f356db100fed4ae47ec20e9384e6d79322eaf
hugosenari <hugosenari@gmail.com>
2022-03-17 18:29:07 -0300

    chore: re-index ../nimpkgs/t/tiger/v0_2_1

--- v0_2_1/flake.nix
+++ v0_2_1/flake.nix
@@ -11,7 +11,7 @@
   inputs.src-tiger-v0_2_1.owner = "~ehmry";
   inputs.src-tiger-v0_2_1.ref   = "refs/tags/v0.2.1";
   inputs.src-tiger-v0_2_1.repo  = "nim_tiger";
-  inputs.src-tiger-v0_2_1.type  = "other";
+  inputs.src-tiger-v0_2_1.type  = "sourcehut";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 

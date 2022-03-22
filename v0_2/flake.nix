7f21e3f33f3b7ae6f819c526b9bdf0852cf07210
hugosenari <hugosenari@gmail.com>
2022-03-22 04:24:10 -0300

    chore: re-index ../nimpkgs/t/tiger

--- v0_2/flake.nix
+++ v0_2/flake.nix
@@ -8,7 +8,7 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs."src-tiger-v0_2".flake = false;
-  inputs."src-tiger-v0_2".ref   = "refs/tags/v0.2";
+  inputs."src-tiger-v0_2".ref   = "v0.2";
   inputs."src-tiger-v0_2".owner = "~ehmry";
   inputs."src-tiger-v0_2".repo  = "nim_tiger";
   inputs."src-tiger-v0_2".type  = "sourcehut";
